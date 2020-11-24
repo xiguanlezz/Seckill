d388101107797cdca428d55745b62196a303c53e
xiguanlezz <1561435010@qq.com>
2020-11-24 20:18:02 +0800

    修改login方法的返回参数为User实体类

--- src/main/java/com/cj/cn/service/IUserService.java
+++ src/main/java/com/cj/cn/service/IUserService.java
@@ -1,10 +1,11 @@
 package com.cj.cn.service;
 
 import com.cj.cn.dto.LoginDTO;
+import com.cj.cn.pojo.User;
 
 public interface IUserService {
     /**
      * 用户登录
      */
-    boolean login(LoginDTO loginDTO);
+    User login(LoginDTO loginDTO);
 }
--- src/main/java/com/cj/cn/service/impl/UserServiceImpl.java
+++ src/main/java/com/cj/cn/service/impl/UserServiceImpl.java
@@ -2,11 +2,10 @@ package com.cj.cn.service.impl;
 
 import com.cj.cn.dto.LoginDTO;
 import com.cj.cn.exception.GlobalException;
-import com.cj.cn.mapper.UserMapper;
+import com.cj.cn.dao.UserMapper;
 import com.cj.cn.pojo.User;
 import com.cj.cn.response.CodeEnum;
 import com.cj.cn.service.IUserService;
-import com.cj.cn.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,16 +16,18 @@ public class UserServiceImpl implements IUserService {
     private UserMapper userMapper;
 
     @Override
-    public boolean login(LoginDTO loginDTO) {
+    public User login(LoginDTO loginDTO) {
         User user = userMapper.selectByPrimaryKey(loginDTO.getMobile());
         if (user == null) {
             throw new GlobalException(CodeEnum.INCORRECT_MOBILE);
         }
         String dbPwd = user.getPassword();
-        String pwd = MD5Util.formPwdToDBPwd(loginDTO.getPassword(), user.getSalt());
+//        String pwd = MD5Util.formPwdToDBPwd(loginDTO.getPassword(), user.getSalt());
+        String pwd = loginDTO.getPassword();
         if (!StringUtils.equals(dbPwd, pwd)) {
             throw new GlobalException(CodeEnum.INCORRECT_PWD);
         }
-        return true;
+
+        return user;
     }
 }
