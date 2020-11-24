561b217efafa49e43aebd1d617c270b4aef49da4
xiguanlezz <1561435010@qq.com>
2020-11-24 21:07:37 +0800

    测试消息队列

--- src/test/java/com/cj/cn/MiaoshaApplicationTests.java
+++ src/test/java/com/cj/cn/MiaoshaApplicationTests.java
@@ -1,13 +1,42 @@
 package com.cj.cn;
 
+import com.cj.cn.pojo.User;
+import com.cj.cn.rabbitmq.MQSender;
+import com.cj.cn.rabbitmq.MiaoshaMessage;
+import com.cj.cn.service.IGoodService;
+import com.cj.cn.util.ConstUtil;
+import com.cj.cn.util.RedisUtil;
+import com.cj.cn.vo.GoodVO;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MiaoshaApplicationTests {
+    @Autowired
+    private RedisUtil redisUtil;
+    @Autowired
+    private IGoodService iGoodService;
+    @Autowired
+    private MQSender mqSender;
 
     @Test
-    void contextLoads() {
+    void testGoodsListJson() {
+        System.out.println(redisUtil.get(ConstUtil.goodsListKey));
+    }
+
+    @Test
+    void testMiaoshaOne() {
+        GoodVO goodVO = new GoodVO();
+        goodVO.setId(1l);
+        int i = iGoodService.reduceStock(goodVO);
+        Assert.assertEquals(1, i);
+    }
+
+    @Test
+    void testRabbitmq() {
+        mqSender.sendMiaoshaMessage(new MiaoshaMessage().setMiaoshaUser(new User().setId(123l)).setGoodsId(1));
     }
 
 }
