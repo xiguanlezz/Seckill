2cff6bae63858dd5ad33c517143efefd7bd79092
xiguanlezz <1561435010@qq.com>
2020-11-24 20:12:05 +0800

    修改原来的mapper包为dao包

--- src/main/java/com/cj/cn/MiaoshaApplication.java
+++ src/main/java/com/cj/cn/MiaoshaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@MapperScan("com.cj.cn.mapper")
+@MapperScan("com.cj.cn.dao")
 @SpringBootApplication
 public class MiaoshaApplication {
 
