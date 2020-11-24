ed0674c1d8ac7e6f3db95fb8262e829920e04ff6
xiguanlezz <1561435010@qq.com>
2020-11-24 20:12:38 +0800

    增加一些常量

---
+++ src/main/java/com/cj/cn/util/ConstUtil.java
@@ -0,0 +1,8 @@
+package com.cj.cn.util;
+
+public class ConstUtil {
+    public final static String cookieName = "miaosha_token";
+    public static final String cookieValuePrefix = "miaosha_";
+    public static final String goodsListKey = "miaosha_goodslist";
+    public static final String goodDetailKeyPrefix = "miaosha_good_detail_";
+}
