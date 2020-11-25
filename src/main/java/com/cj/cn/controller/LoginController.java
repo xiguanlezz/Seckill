c17ddda94cca633efcf3bc0945eed8fea6f11a21
xiguanlezz <1561435010@qq.com>
2020-11-25 13:59:24 +0800

    登录的控制器

--- src/main/java/com/cj/cn/controller/LoginController.java
+++ src/main/java/com/cj/cn/controller/LoginController.java
@@ -1,15 +1,24 @@
 package com.cj.cn.controller;
 
 import com.cj.cn.dto.LoginDTO;
+import com.cj.cn.pojo.User;
 import com.cj.cn.response.ResultResponse;
 import com.cj.cn.service.IUserService;
+import com.cj.cn.util.ConstUtil;
+import com.cj.cn.util.CookieUtil;
+import com.cj.cn.util.JsonUtil;
+import com.cj.cn.util.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Controller
@@ -17,6 +26,8 @@ import javax.validation.Valid;
 public class LoginController {
     @Autowired
     private IUserService iUserService;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @RequestMapping("/to_login")
     public String toLogin() {
@@ -25,10 +36,13 @@ public class LoginController {
 
     @RequestMapping("/do_login")
     @ResponseBody   //SpringMVC默认用的是JSON序列化的方式, 如果这个类没有get方法会报错(猜测底层是反射)
-    public ResultResponse doLogin(@Valid LoginDTO loginDTO) {
+    public ResultResponse doLogin(@Valid LoginDTO loginDTO, HttpServletResponse response) {
         log.info(loginDTO.toString());
-        //登录
-        iUserService.login(loginDTO);
-        return ResultResponse.ok();
+        User user = iUserService.login(loginDTO);
+        assert user != null;
+        String token = ConstUtil.cookieValuePrefix + user.getId();
+        CookieUtil.writeLoginToken(response, token);
+        redisUtil.set(ConstUtil.cookieValuePrefix + user.getId(), JsonUtil.objToStr(user), 30, TimeUnit.MINUTES);
+        return ResultResponse.ok(token);
     }
 }
